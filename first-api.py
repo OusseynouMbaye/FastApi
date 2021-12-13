65eefa0cf0614b1f343636d229eef5030677d9e8
Ouzinem <ouzinem@hotmail.com>
2021-12-12 21:59:21 -0500

    Premier pas avec API CRUD

--- first-api.py
+++ first-api.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 
 app = FastAPI()
+
+products = [
+    {"id": 1, "name": "iPad", "price": 599},
+    {"id": 2, "name": "iPhone", "price": 999},
+    {"id": 3, "name": "iWatch", "price": 699},
+]
 
 
 @app.get("/")
 def index():
-    return {"message": "Hello World"}
+    return {"message": "Hello home"}
+
+
+@app.get("/products/{id}")
+def index(id: int, response: Response):
+    for product in products:
+        if product["id"] == id:
+            return product
+
+    response.status_code = 404
+    return "Product Not found"
